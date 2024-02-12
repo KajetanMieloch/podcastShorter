544039eb20903f7f7a1eadb4176516d8eae20857
KajetanMieloch <91525250+KajetanMieloch@users.noreply.github.com>
2024-02-12 21:17:45 +0100

    Add video proccessing to the podcastShorter.py script
    This commit adds the ability to process video files to the podcastShorter.py
    You may drag and drop a video file onto the script and it will process it
    and output a file with the same name but with the output.avi extension.
    
    There is no audio processing in this commit, but it will be added in the future.

--- podcastShorter.py
+++ podcastShorter.py
@@ -1,4 +1,5 @@
 import sys
+import cv2
 
 if len(sys.argv) != 2:
     print("Usage python podcastShorter.py filename.mp4")
@@ -7,12 +8,25 @@ if len(sys.argv) != 2:
 file = sys.argv[1]
 
 try:
-    with open(file, 'r') as f:
-        zawartosc = f.read()
-        print(zawartosc)
+    video = cv2.VideoCapture(file)
+    # Save the video
+    fps = video.get(cv2.CAP_PROP_FPS)
+    width = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    fourcc = cv2.VideoWriter_fourcc(*'DIVX')
+    out = cv2.VideoWriter('output.avi', fourcc, fps, (width, height))
 except FileNotFoundError:
     print(f"file {file} does not exist.")
     sys.exit(1)
 except:
-    print("Error occured.")
+    print("Error occurred.")
     sys.exit(1)
+
+while True:
+    ret, frame = video.read()
+    if not ret:
+        break
+    out.write(frame)
+
+video.release()
+out.release()
